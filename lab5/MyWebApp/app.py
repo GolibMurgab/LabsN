962ce156685c3f81db84f94298c1be3f6a238e09
Mark <koanec5000@gmail.com>
2023-03-29 23:59:23 +0300

    исправи

--- lab5/MyWebApp/app.py
+++ lab5/MyWebApp/app.py
@@ -1,4 +1,3 @@
-import requests
 from flask import Flask, render_template, request, redirect
 import psycopg2
 
@@ -13,19 +12,18 @@ conn = psycopg2.connect(database="service_db",
 cursor = conn.cursor()
 
 
-@app.route('/login/', methods=['POST','GET'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         if request.form.get("login"):
-            error = "Неправильный логин или пароль, запрещены символы пробела"
             username = request.form.get('username')
             password = request.form.get('password')
             cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
             records = list(cursor.fetchall())
             if ' ' in str(username) or ' ' in str(password) or str(password) == "" or str(username) == "":
-                return render_template('login.html', error=error)
+                return render_template('login.html', error="Неправильный логин или пароль, запрещены символы пробела")
             if len(records) == 0:
-                return render_template('login.html', error1="Нет такого пользователя")
+                return render_template('login.html', error="Нет такого пользователя")
             return render_template('account.html', full_name=records[0][1], username=username, password=password)
         elif request.form.get("registration"):
             return redirect("/registration/")
@@ -38,7 +36,7 @@ def registration():
         name = request.form.get('name').strip()
         login = request.form.get('login').strip()
         password = request.form.get('password').strip()
-        cursor.execute("SELECT * FROM service.users WHERE login=%s", (str(login), ))
+        cursor.execute("SELECT * FROM service.users WHERE login=%s", (str(login),))
         records = list(cursor.fetchall())
         if len(records) != 0:
             return render_template('registration.html', error2="Данный пользователь уже зарегистрирован")
@@ -59,6 +57,7 @@ def registration():
         conn.commit()
         return redirect('/login/')
     return render_template('registration.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
